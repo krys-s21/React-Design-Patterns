f269542ed588afb3654b5d14b542d589cd011bb6
krys-s21 <krystyna.shvets@gmail.com>
2023-10-10 15:32:28 +0100

    Layout Components: added split-screen component

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import {
+  SplitScreen
+} from './SplitScreen';
+
+const LeftHandComponent=()=>{
+  return <h1 style={{backgroundColor:'green'}}>Left!</h1>;
+}
+const RightHandComponent=()=>{
+  return <p style={{backgroundColor:'red'}}>Right!</p>;
+}
+function App() {
+  return (
+  <SplitScreen 
+  left={LeftHandComponent}
+  right={RightHandComponent}
+  />
+  );
+}
+
+export default App;
