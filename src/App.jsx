e561966d0724aca8b87d7add6d01d27724693dfd
krys-s21 <krystyna.shvets@gmail.com>
2023-10-11 11:52:21 +0100

    Layout Component: isolate all layout component exercises into layout folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css';
 import {
   SplitScreenWrapper
-} from './components/SplitScreenWrapper';
+} from './components/layout/SplitScreenWrapper';
 import {
   ListsWrapper
-} from './components/ListsWrapper';
+} from './components/layout/ListsWrapper';
 import {
   Modal
-} from './components/Modal';
+} from './components/layout/Modal';
 
 function App() {
   return (
