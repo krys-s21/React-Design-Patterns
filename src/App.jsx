53dc729f9076763af8016b022ee3e6fe87d464a6
krys-s21 <krystyna.shvets@gmail.com>
2023-10-12 13:00:48 +0100

    Uncontrolled Components: added controlled folder and ControlledForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,40 @@
 import './App.css';
-import axios from 'axios';
-import {
-  SplitScreenWrapper
-} from './components/layout/SplitScreenWrapper';
-import {
-  ListsWrapper
-} from './components/layout/ListsWrapper';
-import {
-  Modal
-} from './components/layout/Modal';
-import {
-  CurrentUserLoader
-} from './components/container/CurrentUserLoader';
-import {
-  UserInfo
-} from './components/container/UserInfo';
-import {
-  UserLoader
-} from './components/container/UserLoader';
-import {
-  ResourceLoader
-} from './components/container/ResourceLoader';
-import {
-  ProductInfo
-} from './components/container/ProductInfo';
-import {
-  DataSourceWrapper
-} from './components/container/DataSourceWrapper';
+// import axios from 'axios';
+// import {
+//   SplitScreenWrapper
+// } from './components/layout/SplitScreenWrapper';
+// import {
+//   ListsWrapper
+// } from './components/layout/ListsWrapper';
+// import {
+//   Modal
+// } from './components/layout/Modal';
+// import {
+//   CurrentUserLoader
+// } from './components/container/CurrentUserLoader';
+// import {
+//   UserInfo
+// } from './components/container/UserInfo';
+// import {
+//   UserLoader
+// } from './components/container/UserLoader';
+// import {
+//   ResourceLoader
+// } from './components/container/ResourceLoader';
+// import {
+//   ProductInfo
+// } from './components/container/ProductInfo';
+// import {
+//   DataSourceWrapper
+// } from './components/container/DataSourceWrapper';
 
-import { UncontrolledForm } from './components/uncontrolled/UncontrolledForm';
+// import { UncontrolledForm } from './components/uncontrolled/UncontrolledForm';
+import { ControlledForm } from './components/controlled/ControlledForm';
 
 function App() {
   return (
     <>
-      <UncontrolledForm />
+      <ControlledForm />
     </>
   );
 }
