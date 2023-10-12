c799f51956924ae9f11f4f37424eb18b4f3e84b9
krys-s21 <krystyna.shvets@gmail.com>
2023-10-12 14:39:19 +0100

    Uncontrolled Components: improved UncontrolledOnboardingFlow

--- src/components/uncontrolled/UncontrolledOnboardingFlow.jsx
+++ src/components/uncontrolled/UncontrolledOnboardingFlow.jsx
@@ -6,8 +6,22 @@ export const UncontrolledOnboardingFlow = ({children, onFinish}) => {
 
     const currentChild = React.Children.toArray(children)[currentIndex];
 
-    const goToNext = () => {
-        setCurrentIndex(currentIndex + 1);
+    const goToNext = (stepData) => {
+        const nextIndex = currentIndex + 1;
+        const updatedData = {
+            ...onboardingData,
+            ...stepData,
+        };
+
+        console.log(updatedData);
+
+        if (nextIndex < children.length){
+            setCurrentIndex(nextIndex);
+        }else{
+            onFinish(updatedData);
+        }
+        
+        setOnboardingData(updatedData);
     }
     if (React.isValidElement(currentChild)){
         return React.cloneElement(currentChild,{goToNext});
--- src/components/uncontrolled/UncontrolledOnboardingFlowWrapper.jsx
+++ src/components/uncontrolled/UncontrolledOnboardingFlowWrapper.jsx
@@ -5,24 +5,28 @@ import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
 const StepOne = ({goToNext}) => (
     <>
         <h1>Step 1</h1>
-        <button onClick={goToNext}>Next</button>
+        <button onClick={() => goToNext({name: 'John Doe'})}>Next</button>
     </>
 );
 const StepTwo = ({goToNext}) => (
     <>
         <h1>Step 2</h1>
-        <button onClick={goToNext}>Next</button>
+        <button onClick={() => goToNext({age: 100})}>Next</button>
     </>
 );
 const StepThree = ({goToNext}) => (
     <>
         <h1>Step 3</h1>
-        <button onClick={goToNext}>Next</button>
+        <button onClick={() => goToNext({hairColor: 'brown'})}>Next</button>
     </>
 );
+
 export const UncontrolledOnboardingFlowWrapper = () => {
     return (
-       <UncontrolledOnboardingFlow>
+       <UncontrolledOnboardingFlow onFinish={ data => {
+        console.log(data);
+        alert("Onboarding Complete!")
+       }}>
         <StepOne />
         <StepTwo />
         <StepThree />
