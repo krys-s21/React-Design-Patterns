97ddcbe803f49def9800d6f0a55d0a7597bef489
krys-s21 <krystyna.shvets@gmail.com>
2023-10-19 15:16:21 +0100

    High-Order Components: added withEditableResource to finish HOC practice

--- src/components/highOrder/UserInfoForm.jsx
+++ src/components/highOrder/UserInfoForm.jsx
@@ -1,6 +1,7 @@
-import { withEditableUser } from './withEditableUser';
-
-export const UserInfoForm = withEditableUser(({user, onChangeUser, onSaveUser, onResetUser})=>{
+//import { withEditableUser } from './withEditableUser';
+import { withEditableResource } from './withEditableResource';
+//export const UserInfoForm = withEditableUser(({user, onChangeUser, onSaveUser, onResetUser})=>{
+export const UserInfoForm = withEditableResource(({user, onChangeUser, onSaveUser, onResetUser})=>{
     const {name, age, hairColor}=user ||{};
 
     return user ? (
@@ -15,4 +16,4 @@ export const UserInfoForm = withEditableUser(({user, onChangeUser, onSaveUser, o
         <button onClick={onSaveUser}>Save Changes</button>
         </>
     ) : <p>Loading...</p>
-}, '/2');
+}, '/users/2','user');
